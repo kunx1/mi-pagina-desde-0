62fda36737ebec5e2477b5aad9a720968ffd4bc6
Rodrigo Lezana <75607037+kunx1@users.noreply.github.com>
2023-03-10 04:38:49 -0300

    menu arreglado

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const btn = document.querySelector('.menu-button')
 const nav = document.querySelector('.nav')
+
+const menu = document.querySelector('.menu-button')
+
+
+
 btn.addEventListener('click', ()=>{
     nav.classList.toggle('activo')
+    menu.classList.toggle('activox')
 })
+
+
+
+// const menu = document.querySelector('.menu-button')
+
+// menu.addEventListener('click',() =>{
+// menu.classList.toggle("activox")
+// })
 

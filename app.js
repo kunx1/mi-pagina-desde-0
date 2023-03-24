fef75a998857389724181d7786f63ef34dd9fd1e
Rodrigo Lezana <75607037+kunx1@users.noreply.github.com>
2023-03-24 05:07:10 -0300

    contacto no terminado

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const btn = document.querySelector('.menu-button')
-const nav = document.querySelector('.nav')
+// const btn = document.querySelector('.menu-button')
+// const nav = document.querySelector('.nav')
 
-const menu = document.querySelector('.menu-button')
+// const menu = document.querySelector('.menu-button')
 
 
 
-btn.addEventListener('click', ()=>{
-    nav.classList.toggle('activo')
-    menu.classList.toggle('activox')
-})
+// btn.addEventListener('click', ()=>{
+//     nav.classList.toggle('activo')
+//     menu.classList.toggle('activox')
+// })
+
+const menuButton = document.querySelector('.menu-button');
+const nav = document.querySelector('.nav');
+
+menuButton.addEventListener('click', () => {
+  nav.classList.toggle('activo');
+  menuButton.classList.toggle('activox');
+});
+
 
 
 
